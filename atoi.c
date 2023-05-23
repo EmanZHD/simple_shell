9517b1e8150852b178d0544d9eff536199df7f8b
EmaneZHD <zahidimane1@gmail.com>
2023-05-23 17:24:07 +0000

    done

--- atoi.c
+++ atoi.c
@@ -3,71 +3,70 @@
 /****************** Done By Imane ZAHID & Ghita BOUZRBAY ******************/
 
 /**
- * interactive - returns true if shell is interactive mode
- * @info: struct address
- *
- * Return: 1 if interactive mode, 0 otherwise
+ * intractive - this function returns true if shell is interactive mode
+ * @info: the struct address
+ * Return: return 1 if interactive mode, 0 otherwise
  */
-int interactive(info_t *info)
+int intractive(info_t *info)
 {
-	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+	return (isatty(STDIN_FILENO) && info->readfdd <= 2);
 }
 
 /**
- * is_delim - checks if character is a delimeter
- * @c: the char to check
- * @delim: the delimeter string
- * Return: 1 if true, 0 if false
+ * is_delm - this function checks if character is a delimeter
+ * @ch: char to check
+ * @delm: delimeter string
+ * Return: return 1 if true, 0 if false
  */
-int is_delim(char c, char *delim)
+int is_delm(char ch, char *delm)
 {
-	while (*delim)
-		if (*delim++ == c)
+	while (*delm)
+		if (*delm++ == ch)
 			return (1);
 	return (0);
 }
 
 /**
- * _isalpha - checks for alphabetic character
- * @c: The character to input
- * Return: 1 if c is alphabetic, 0 otherwise
+ * _alpha - this function checks for alphabetic character
+ * @ch: character to input
+ * Return: return 1 if c is alphabetic,and 0 otherwise
  */
 
-int _isalpha(int c)
+int _alpha(int ch)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
 		return (1);
 	else
 		return (0);
 }
 
 /**
- * _atoi - converts a string to an integer
- * @s: the string to be converted
- * Return: 0 if no numbers in string, converted number otherwise
+ * _atoi - this function converts a string to an integer
+ * @st: string to be converted
+ * Return: return 0 if no numbers in string, converted number otherwise
  */
 
-int _atoi(char *s)
+int _atoi(char *st)
 {
-	int i, sign = 1, flag = 0, output;
+	int i, signl = 1, flg = 0, output;
 	unsigned int result = 0;
 
-	for (i = 0; s[i] != '\0' && flag != 2; i++)
+	for (i = 0; st[i] != '\0' && flg != 2; i++)
 	{
-		if (s[i] == '-')
-			sign *= -1;
+		if (st[i] == '-')
+			signl *= -1;
 
-		if (s[i] >= '0' && s[i] <= '9')
+		if (st[i] >= '0' && st[i] <= '9')
 		{
-			flag = 1;
+			flg = 1;
 			result *= 10;
-			result += (s[i] - '0');
+			result += (st[i] - '0');
 		}
-		else if (flag == 1)
-			flag = 2;
+		else if (flg == 1)
+			flg = 2;
 	}
 
-	if (sign == -1)
+	if (signl == -1)
 		output = -result;
 	else
 		output = result;
