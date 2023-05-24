c801697df38273b75bbce16ca8eba1e0cf43946c
EmaneZHD <zahidimane1@gmail.com>
2023-05-24 13:48:05 +0000

    done

--- main.c
+++ main.c
@@ -1,44 +1,45 @@
 #include "shell.h"
 
+/****************** Done By Imane ZAHID & Ghita BOUZRBAY ******************/
+
 /**
- * main - entry point
- * @ac: arg count
- * @av: arg vector
- *
- * Return: 0 on success, 1 on error
+ * main - this function entry point
+ * @ac: the arg count
+ * @av: the arg vector
+ * Return: return 0 on success, and 1 on error
  */
 int main(int ac, char **av)
 {
 	info_t info[] = { INFO_INIT };
-	int fd = 2;
+	int fdd = 2;
 
 	asm ("mov %1, %0\n\t"
 			"add $3, %0"
-			: "=r" (fd)
-			: "r" (fd));
+			: "=r" (fdd)
+			: "r" (fdd));
 
 	if (ac == 2)
 	{
-		fd = open(av[1], O_RDONLY);
-		if (fd == -1)
+		fdd = open(av[1], O_RDONLY);
+		if (fdd == -1)
 		{
 			if (errno == EACCES)
 				exit(126);
 			if (errno == ENOENT)
 			{
-				_eputs(av[0]);
-				_eputs(": 0: Can't open ");
-				_eputs(av[1]);
-				_eputchar('\n');
-				_eputchar(BUF_FLUSH);
+				_epts(av[0]);
+				_epts(": 0: :( Can't open ");
+				_epts(av[1]);
+				_eptchar('\n');
+				_eptchar(BUF_FLUSH);
 				exit(127);
 			}
 			return (EXIT_FAILURE);
 		}
-		info->readfd = fd;
+		info->readfdd = fdd;
 	}
-	populate_env_list(info);
-	read_history(info);
+	populate_envr_list(info);
+	read_histry(info);
 	hsh(info, av);
 	return (EXIT_SUCCESS);
 }
