6789c327ffdaa336f4038347c5cdc26f049e5e79
EmaneZHD <zahidimane1@gmail.com>
2023-05-24 14:12:07 +0000

    done

--- prsr.c
+++ prsr.c
@@ -1,18 +1,19 @@
 #include "shell.h"
 
+/****************** Done By Imane ZAHID & Ghita BOUZRBAY ******************/
+
 /**
- * is_cmd - determines if a file is an executable command
- * @info: the info struct
- * @path: path to the file
- *
- * Return: 1 if true, 0 otherwise
+ * is_cmd - this function determines if a file is an executable command
+ * @info: info struct
+ * @pat: the path to the file
+ * Return: return 1 if true, 0 otherwise
  */
-int is_cmd(info_t *info, char *path)
+int is_cmd(info_t *info, char *pat)
 {
 	struct stat st;
 
 	(void)info;
-	if (!path || stat(path, &st))
+	if (!pat || stat(pat, &st))
 		return (0);
 
 	if (st.st_mode & S_IFREG)
@@ -23,64 +24,62 @@ int is_cmd(info_t *info, char *path)
 }
 
 /**
- * dup_chars - duplicates characters
- * @pathstr: the PATH string
- * @start: starting index
- * @stop: stopping index
- *
- * Return: pointer to new buffer
+ * dup_chars - this function duplicates characters
+ * @patstrg: the PATH string
+ * @begi: starting index
+ * @end: stopping index
+ * Return: return pointer to new buffer
  */
-char *dup_chars(char *pathstr, int start, int stop)
+char *dup_chars(char *patstrg, int begi, int end)
 {
-	static char buf[1024];
-	int i = 0, k = 0;
+	static char bufr[1024];
+	int a = 0, j = 0;
 
-	for (k = 0, i = start; i < stop; i++)
-		if (pathstr[i] != ':')
-			buf[k++] = pathstr[i];
-	buf[k] = 0;
-	return (buf);
+	for (j = 0, a = begi; a < end; a++)
+		if (patstrg[a] != ':')
+			bufr[j++] = patstrg[a];
+	bufr[j] = 0;
+	return (bufr);
 }
 
 /**
- * find_path - finds this cmd in the PATH string
- * @info: the info struct
- * @pathstr: the PATH string
+ * fnd_pat - this function finds this cmd in the PATH string
+ * @info: info struct
+ * @patstrg: PATH string
  * @cmd: the cmd to find
- *
- * Return: full path of cmd if found or NULL
+ * Return: full pat of cmd if found or NULL
  */
-char *find_path(info_t *info, char *pathstr, char *cmd)
+char *fnd_pat(info_t *info, char *patstrg, char *cmd)
 {
-	int i = 0, curr_pos = 0;
-	char *path;
+	int a = 0, curr_pos = 0;
+	char *pat;
 
-	if (!pathstr)
+	if (!patstrg)
 		return (NULL);
-	if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
+	if ((_strglen(cmd) > 2) && starts_with(cmd, "./"))
 	{
 		if (is_cmd(info, cmd))
 			return (cmd);
 	}
 	while (1)
 	{
-		if (!pathstr[i] || pathstr[i] == ':')
+		if (!patstrg[a] || patstrg[a] == ':')
 		{
-			path = dup_chars(pathstr, curr_pos, i);
-			if (!*path)
-				_strcat(path, cmd);
+			pat = dup_chars(patstrg, curr_pos, a);
+			if (!*pat)
+				_strgcat(pat, cmd);
 			else
 			{
-				_strcat(path, "/");
-				_strcat(path, cmd);
+				_strgcat(pat, "/");
+				_strgcat(pat, cmd);
 			}
-			if (is_cmd(info, path))
-				return (path);
-			if (!pathstr[i])
+			if (is_cmd(info, pat))
+				return (pat);
+			if (!patstrg[a])
 				break;
-			curr_pos = i;
+			curr_pos = a;
 		}
-		i++;
+		a++;
 	}
 	return (NULL);
 }
